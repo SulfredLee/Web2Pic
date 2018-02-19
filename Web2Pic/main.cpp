ad5a3e5d1bfb9aff3cd76dc4c6f8f660678611df
sulfred <sflee1112@gmail.com>
2018-02-19 19:39:58 +0800

    Single file version

--- Web2Pic/main.cpp
+++ Web2Pic/main.cpp
@@ -1,28 +1,32 @@
 #include <QtWidgets/QApplication>
-#include "webwindow.h"
+#include <QtWebEngineWidgets/QWebEngineView>
 
 int main(int argc, char *argv[]){
     QApplication a(argc, argv);
-    WebWindow ww;
 
-    ww.DoCapture();
-    //QWebEngineView w;
-    //QImage image(height, width, QImage::Format_RGB32);
-    //QRegion rg(left, top, width, height);
-    //QPainter painter(&image);
-    ////w->setUrl(QUrl("https://www.google.com/search?q=0001"));
-    //w.setUrl(QUrl("https://www.yahoo.com"));
-    //w.resize(1024, 750);
-    //w.show();
-    //w.render(&painter, QPoint(), rg);
-    ////w.page()->view()->render(&painter, QPoint(), rg);
-    //painter.end();
-    ////QByteArray ba;
-    ////QBuffer bu(&ba);
-    ////bu.open(QBuffer::ReadWrite);
-    ////bu.close();
-    ////image.save(&bu, "PNG", 80);
-    //image.save("test.png", "PNG", 80);
+    QWebEngineView* w = new QWebEngineView();
+    //w->setUrl(QUrl("https://www.google.com/search?q=0001"));
+    w->setUrl(QUrl("https://www.yahoo.com"));
+    w->resize(1024, 750);
+    w->show();
+    QObject::connect(w, &QWebEngineView::loadFinished,
+                     [w](bool bOk)
+                     {
+                         int height, width, left, top;
+                         height = 600;
+                         width = 800;
+                         left = 0;
+                         top = 0;
+
+                         QImage image(height, width, QImage::Format_RGB32);
+                         QRegion rg(left, top, width, height);
+                         QPainter painter(&image);
+                         //w.render(&painter, QPoint(), rg);
+                         w->page()->view()->render(&painter, QPoint(), rg);
+                         painter.end();
+                         //image.save(&bu, "PNG", 80);
+                         image.save("test.png", "PNG", 80);
+                     });
 
     return a.exec();
 }
